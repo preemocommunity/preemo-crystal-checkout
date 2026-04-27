b42c23c8e50fed6fce0a3bfe7a3d8900a8ce7b06
Prëmo <95042253+preemocommunity@users.noreply.github.com>
2026-04-27 09:21:46 +0800

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -4,7 +4,7 @@ const PRICE_IDS = {
   1:  "price_1TQdhBDWURdmEJUGAu0beHL1",
   3:  "price_1TQdhODWURdmEJUGuzLEQsKe",
   6:  "price_1TQdhYDWURdmEJUGHuDaK3Kk",
-  12: "price_1TQU1ZDWURdmEJUGm2Hfiemn",
+  12: "price_1TQdhgDWURdmEJUGYkANx8HV",
 };
  
 module.exports = async (req, res) => {
