c00fe1ec9b1f34ef73144ab6858d2afe49507be3
Prëmo <95042253+preemocommunity@users.noreply.github.com>
2026-04-27 09:20:54 +0800

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,7 +1,7 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
  
 const PRICE_IDS = {
-  1:  "price_1TQU0nDWURdmEJUGwJnitULZ",
+  1:  "price_1TQdhBDWURdmEJUGAu0beHL1",
   3:  "price_1TQU18DWURdmEJUGP9ZkYvUa",
   6:  "price_1TQU1ODWURdmEJUGQMAlvUHa",
   12: "price_1TQU1ZDWURdmEJUGm2Hfiemn",
